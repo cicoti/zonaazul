aad1b13bc0cd66c16c919c9bb9352ee7f3c9d7a6
= <silvio.cicoti@gmail.com>
2017-09-21 10:25:35 -0300

    Correcao

--- src/main/java/br/com/zonaazul/delegate/VagaDelegate.java
+++ src/main/java/br/com/zonaazul/delegate/VagaDelegate.java
@@ -11,8 +11,8 @@ import br.com.zonaazul.util.BusinessServiceException;
 import br.com.zonaazul.util.Haversine;
 import br.com.zonaazul.util.ServiceException;
 
-public class VagaDelegate implements Serializable { 
-	
+public class VagaDelegate implements Serializable {
+
 	private static final long serialVersionUID = -5916801404195287097L;
 
 	@Inject private VagaService vagaService;
@@ -24,11 +24,11 @@ public class VagaDelegate implements Serializable {
 				return true;
 			}
 		}
-		
-		throw new BusinessServiceException("A vaga solicitada não existe.");
-		
+
+		throw new BusinessServiceException("A vaga solicitada n\u00e3o existe.");
+
 	}
-	
+
 	public boolean pesquisarVagaLivre(Vaga v) throws ServiceException{
 		List<Vaga> listaVagaLivre = this.listaVagaLivre();
 		for(Vaga vaga : listaVagaLivre){
@@ -36,11 +36,11 @@ public class VagaDelegate implements Serializable {
 				return true;
 			}
 		}
-		
-		throw new BusinessServiceException("A vaga solicitada não está livre.");
-		
+
+		throw new BusinessServiceException("A vaga solicitada n\u00e3o est\u00e1 livre.");
+
 	}
-	
+
 	public boolean pesquisarVagaProximidade(Vaga v) throws ServiceException {
 		List<Vaga> listaVagaLivre = this.listaVagaLivre();
 		for(Vaga vaga : listaVagaLivre){
@@ -48,21 +48,21 @@ public class VagaDelegate implements Serializable {
 
 				//Haversine.distance(startLat, startLong, endLat, endLong)
 				int distancia = (int) (Haversine.distance(Double.parseDouble(v.getNrLatitude().replaceAll(",", ".")), Double.parseDouble(v.getNrLongitude().replaceAll(",", ".")), Double.parseDouble(vaga.getNrLatitude().replaceAll(",", ".")), Double.parseDouble(vaga.getNrLongitude().replaceAll(",", "."))));
-							
+
 				System.out.println("Distancia :" + distancia);
-				
+
 				if(distancia > 100){
-					throw new BusinessServiceException("Você está distante da vaga solicitada.");
+					throw new BusinessServiceException("Voc\u00ea est\u00e1 distante da vaga solicitada.");
 				} else {
 					return true;
 				}
-								
+
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public Vaga buscarVaga(Vaga v) throws ServiceException{
 		List<Vaga> listaVaga = this.listaVaga();
 		for(Vaga vaga : listaVaga){
@@ -70,15 +70,15 @@ public class VagaDelegate implements Serializable {
 				return vaga;
 			}
 		}
-		
-		throw new BusinessServiceException("A vaga solicitada não foi encontrada.");
-		
+
+		throw new BusinessServiceException("A vaga solicitada n\u00e3o foi encontrada.");
+
 	}
 
 	public List<Vaga> listaVagaLivre() throws ServiceException {
 		return vagaService.listaVagaLivre();
 	}
-	
+
 	public List<Vaga> listaVaga() throws ServiceException {
 		return vagaService.listaVaga();
 	}
